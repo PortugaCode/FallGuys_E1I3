ffc157e592031989cbe6650c67890f4f1f3aa6e4
오중근 <148051315+PortugaCode@users.noreply.github.com>
2023-12-15 17:21:01 +0900

    [Feat] PlayerMove

--- Assets/3.Script/Player/PlayerControl_CineTest.cs
+++ Assets/3.Script/Player/PlayerControl_CineTest.cs
@@ -10,7 +10,11 @@ public class PlayerControl_CineTest : MonoBehaviour
 
 	[Header("Movement")]
 	public float speed = 5.0f;
+	public float rotationSpeed = 10.0f;
 	public float jumpPower = 5.0f;
+
+	[Header("Camera")]
+	[SerializeField] private Transform CameraTransform;
 
 	[Header("IsGround")]
 	public LayerMask playerLayer;
@@ -19,14 +23,15 @@ public class PlayerControl_CineTest : MonoBehaviour
 	[SerializeField] private Material redMaterial;
 
 	private bool isGround;
-
 	private int layerMask;
 
-	private Vector3 targetDirection;
+
+	private Vector3 moveDirection;
+	private float x;
+	private float z;
 
 	private void Start()
 	{
-
 		animator = GetComponent<Animator>();
 		rb = GetComponent<Rigidbody>();
 		layerMask = ~playerLayer.value;
@@ -34,6 +39,8 @@ public class PlayerControl_CineTest : MonoBehaviour
 
     private void Update()
     {
+		MyInput();
+
 		if (Input.GetKeyDown(KeyCode.Space) && isGround)
 		{
 			animator.SetTrigger("Jump");
@@ -43,21 +50,73 @@ public class PlayerControl_CineTest : MonoBehaviour
 		isGround = Physics.OverlapSphere(transform.position, 0.05f, layerMask).Length > 0;
 	}
 
+	private void MyInput()
+    {
+		z = Input.GetAxis("Vertical");
+		x = Input.GetAxis("Horizontal");
+	}
+
     private void FixedUpdate()
 	{
-
-		float z = Input.GetAxis("Vertical");
-		float x = Input.GetAxis("Horizontal");
-
+		Move();
+		Rotate();
 		float movement = Mathf.Abs(x) + Mathf.Abs(z);
 		animator.SetFloat("Speed", movement);
+	}
 
-		targetDirection = new Vector3(x, 0, z);
+	private void Move()
+    {
+        moveDirection = CameraTransform.forward * z;
+        moveDirection += CameraTransform.right * x;
+		moveDirection *= speed * Time.deltaTime;
+		moveDirection.y = rb.velocity.y;
 
-		if (!(x == 0 && z == 0))
+        if (!(x == 0 && z == 0))
         {
-			rb.velocity = new Vector3(x * speed, rb.velocity.y, z * speed);
-			transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(targetDirection), Time.deltaTime * 10.0f);
+			//rb.MovePosition(rb.position + moveDirection);
+			rb.velocity = moveDirection;
+        }
+
+
+        /*		float myX = Input.GetAxis("Horizontal");
+                float myZ = Input.GetAxis("Vertical");
+
+                Vector3 newDirection = CameraTransform.forward * myZ;
+                newDirection += CameraTransform.right * myX;
+
+
+                if (!(x == 0 && z == 0))
+                {
+                    rb.velocity = newDirection * speed * Time.deltaTime;
+                }*/
+
+    }
+
+
+	private void Rotate()
+	{
+		if (!(x == 0 && z == 0))
+		{
+			Vector3 direction = new Vector3(moveDirection.x, 0, moveDirection.z);
+			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotationSpeed);
 		}
 	}
+
+
+
+
+
+
+    private void OnApplicationFocus(bool focus)
+    {
+        if(focus)
+        {
+			Cursor.lockState = CursorLockMode.Locked;
+        }
+		else
+        {
+			Cursor.lockState = CursorLockMode.None;
+		}
+    }
+
 }
